928dc8ff054e89674eebc47b98dcf02695c2811a
santiago <smelomedina05@gmail.com>
2021-03-31 18:40:52 -0500

    updated constants for backend database connection

--- backend/const/conn.py
+++ backend/const/conn.py
@@ -3,8 +3,8 @@ import os
 
 env = os.environ
 
-DB_HOST = env.get("DB_HOST")
-DB_PORT = env.get("DB_PORT")
-DB_PASS = env.get("DB_PASS")
+DB_HOST = env.get("DB_HOST") if env.get("DB_HOST") else "0.0.0.0"
+DB_PORT = env.get("DB_PORT") if env.get("DB_PORT") else "4000"
+DB_PASS = env.get("DB_PASS") if env.get("DB_PASS") else "123" 
 DB_USER = env.get("DB_USER") if env.get("DB_USER") else "root"
-DB_NAME = env.get("DB_NAME")
+DB_NAME = env.get("DB_NAME") if env.get("DB_NAME") else "aforo"
